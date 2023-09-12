be5a1ae13d346335195ef25356ca29b351f4060e
Kirill Markin <kmarkindev@gmail.com>
2023-09-12 17:14:27 +0300

    feat: created base project structure

---
+++ Sources/Main.cpp
@@ -0,0 +1,13 @@
+#include <QApplication>
+#include <QPushButton>
+
+int main(int argc, char* argv[])
+{
+	QApplication a(argc, argv);
+
+	QPushButton button("Hello world!", nullptr);
+	button.resize(200, 100);
+	button.show();
+
+	return QApplication::exec();
+}
