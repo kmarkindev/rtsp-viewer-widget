410f13d59ceb0b89671a9bdd1d3d3711b451bdb2
Kirill Markin <kmarkindev@gmail.com>
2023-11-26 17:11:08 +0300

    feat: Remove everything related to projects and leave only camera widget

--- Sources/Main.cpp
+++ Sources/Main.cpp
@@ -1,7 +1,7 @@
 #include <QApplication>
 #include <gst/gst.h>
 #include "Utils/GLibMainLoopAdapter.h"
-#include "Widgets/QProjectSelectorWindow/QProjectSelectorWindow.h"
+#include "Widgets/QCameraViewer/QCameraViewer.h"
 
 void initializeGStreamer(int argc, char* argv[])
 {
@@ -17,8 +17,14 @@ int main(int argc, char* argv[])
 
 	initializeGStreamer(argc, argv);
 
-	auto selectorWindow = new QProjectSelectorWindow {};
-	selectorWindow->show();
+	if(argc < 2) {
+		return -1;
+	}
+
+	QByteArray address = argv[1];
+
+	QCameraViewer viewer {};
+	viewer.start(address);
 
 	return QApplication::exec();
 }
--- Sources/Project/ProjectSaveLoad.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <QString>
-
-class Project;
-
-Project loadFromFile(QString filePath, bool& error);
-
-void saveToFile(const Project& project, QString filePath, bool& error);
